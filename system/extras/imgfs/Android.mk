7a847b63283137d98d2d9242ecb4896e5f7a4445
Chris Long <christopherglong@mac.com>
2013-03-13 12:44:31 -0400

    Initial commit of image-based FUSE filesystem

---
+++ system/extras/imgfs/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := imgfs.c
+LOCAL_C_INCLUDES := system/extras/fuse/include
+LOCAL_C_FLAGS := -D _FILE_OFFSET_BITS=64
+LOCAL_SHARED_LIBRARIES := libc libfuse
+LOCAL_MODULE := imgfs
+include $(BUILD_EXECUTABLE)
