6cd7e7a77e564f06d911ba5e79385d8ac32dfc49
Chris Long <christopherglong@mac.com>
2013-03-13 12:56:04 -0400

    Remove unnecessary portions

--- system/extras/imgfs/imgfs.c
+++ system/extras/imgfs/imgfs.c
@@ -6,37 +6,33 @@
     See the file COPYING.
 */
 
-#define FUSE_USE_VERSION 22
 #include <fuse.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
 
-static const char *hello_str = "Hello World!\n";
-static const char *hello_path = "/hello";
+#define FUSE_USE_VERSION 22
 
-static int hello_getattr(const char *path, struct stat *stbuf)
+static int imgfs_getattr(const char *path, struct stat *stbuf)
 {
     int res = 0;
 
     memset(stbuf, 0, sizeof(struct stat));
-    if(strcmp(path, "/") == 0) {
+    if(strcmp(path, "/") == 0)
+    {
         stbuf->st_mode = S_IFDIR | 0755;
         stbuf->st_nlink = 2;
     }
-    else if(strcmp(path, hello_path) == 0) {
-        stbuf->st_mode = S_IFREG | 0444;
-        stbuf->st_nlink = 1;
-        stbuf->st_size = strlen(hello_str);
-    }
     else
+    {
         res = -ENOENT;
+    }
 
     return res;
 }
 
-static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+static int imgfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
                          off_t offset, struct fuse_file_info *fi)
 {
     (void) offset;
@@ -47,49 +43,29 @@ static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 
     filler(buf, ".", NULL, 0);
     filler(buf, "..", NULL, 0);
-    filler(buf, hello_path + 1, NULL, 0);
 
     return 0;
 }
 
-static int hello_open(const char *path, struct fuse_file_info *fi)
+static int imgfs_open(const char *path, struct fuse_file_info *fi)
 {
-    if(strcmp(path, hello_path) != 0)
-        return -ENOENT;
-
-    if((fi->flags & 3) != O_RDONLY)
-        return -EACCES;
-
-    return 0;
+    return -ENOENT;
 }
 
-static int hello_read(const char *path, char *buf, size_t size, off_t offset,
+static int imgfs_read(const char *path, char *buf, size_t size, off_t offset,
                       struct fuse_file_info *fi)
 {
-    size_t len;
-    (void) fi;
-    if(strcmp(path, hello_path) != 0)
-        return -ENOENT;
-
-    len = strlen(hello_str);
-    if (offset < len) {
-        if (offset + size > len)
-            size = len - offset;
-        memcpy(buf, hello_str + offset, size);
-    } else
-        size = 0;
-
-    return size;
+    return -ENOENT;
 }
 
-static struct fuse_operations hello_oper = {
-    .getattr	= hello_getattr,
-    .readdir	= hello_readdir,
-    .open	= hello_open,
-    .read	= hello_read,
+static struct fuse_operations imgfs_oper = {
+    .getattr	= imgfs_getattr,
+    .readdir	= imgfs_readdir,
+    .open	= imgfs_open,
+    .read	= imgfs_read,
 };
 
 int main(int argc, char *argv[])
 {
-    return fuse_main(argc, argv, &hello_oper);
+    return fuse_main(argc, argv, &imgfs_oper);
 }
