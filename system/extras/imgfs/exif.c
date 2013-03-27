268637d12ecac06f4d48ba399c5dc24168f25e54
Chris Long <christopherglong@mac.com>
2013-03-26 21:10:43 -0400

    Find latitude and longitude, not X and Y resolutions

--- system/extras/imgfs/exif.c
+++ system/extras/imgfs/exif.c
@@ -1,34 +1,50 @@
 #define _XOPEN_SOURCE
 
-#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <libexif/exif-data.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <string.h>
 
 #include "exif_ops.h"
 
 static const int BUFFER_SIZE = 1024;
 
-int get_xy_res_path(char *filename, char *buf, unsigned int len)
+static void trim_spaces(char *buf)
 {
-   char s_buf[BUFFER_SIZE];
-   s_buf[0] = '\0';
-   buf[0] = '\0';
-   strcat(buf, "/Resolution/");
-   ExifData *edata = exif_data_new_from_file(filename);
-   if(edata == NULL)
-   {
-      /* the file has no exif data */
-      return -1;
-   }
-   ExifEntry *d_entry = exif_content_get_entry(edata->ifd[EXIF_IFD_0],EXIF_TAG_X_RESOLUTION);
-   exif_entry_get_value(d_entry, s_buf, BUFFER_SIZE);
-   strcat(buf, s_buf);
-   strcat(buf, "X");
-   d_entry = exif_content_get_entry(edata->ifd[EXIF_IFD_0],EXIF_TAG_Y_RESOLUTION);
-  strcat(buf, s_buf);
-   return 0;
+	char *s = buf - 1;
+	for (; *buf; ++buf)
+		if (*buf != ' ')
+			s = buf;
+	*++s = 0;
+}
+
+static int get_tag(char *filename, ExifIfd ifd, ExifTag tag, char *buf)
+{
+	char s_buf[BUFFER_SIZE];
+		
+	ExifData *d = exif_data_new_from_file(filename);
+	if (d == NULL)
+		return -1;
+	
+	ExifEntry *entry = exif_content_get_entry(d->ifd[ifd], tag);
+	if (entry) {
+		exif_entry_get_value(entry, s_buf, sizeof(s_buf));
+		trim_spaces(s_buf);
+		strcat(buf, s_buf);
+		exif_data_unref(d);
+		return 0;
+	} else {
+		exif_data_unref(d);
+		return -1;
+	}
+}
+
+int get_latitude(char *filename, char *buf)
+{
+	return get_tag(filename, EXIF_IFD_0, EXIF_TAG_GPS_LATITUDE, buf);
+}
+
+int get_longitude(char *filename, char *buf)
+{
+	return get_tag(filename, EXIF_IFD_0, EXIF_TAG_GPS_LONGITUDE, buf);
 }
--- system/extras/imgfs/exif.h
+++ system/extras/imgfs/exif.h
@@ -1,6 +1,7 @@
 #ifndef __EXIF_H__
 #define __EXIF_H__
 
-int get_xy_res_path(char *filename, char *buf, unsigned int len);
+int get_latitude(char *filename, char *buf);
+int get_longitude(char *filename, char *buf);
 
 #endif
